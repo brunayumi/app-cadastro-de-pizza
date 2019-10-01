5a33df7d4ef63179f6f1a9e4e01507a38effcca2
brunayumi <bruuhna.84@gmail.com>
2019-10-02 03:34:07 +0900

    modificação do java

--- www/components/acao.js
+++ www/components/acao.js
@@ -3,3 +3,22 @@ $(document).on("click", "#listar", function(){
   $(location).attr("href", "listar.html");
 });
 
+$(document).on("click","#cadastrar", function(){
+  var parametros ={
+    "sabor":$("$sabor").val(""),
+    "valor":$("$valor").val("")
+  };
+  $.ajax({
+    type:"post", //como vou enviar os dados ao servidor
+    url:"https://dsetec.000webhostapp.com/cadastra.php",//para onde vou enviar
+    data:parametros,//o que eu vou enviar
+    //caso esteja certo executa esse codigo
+    success: function(data){
+      navigator.notification.alert(data);
+    },
+    //caso esteja errado executa esse codigo
+    error: function(data){
+      navigator.notification.alert("Erro ao cadastrar!");
+    }
+  });
+}); 
